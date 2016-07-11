2be8cabaf4eb31f1ab12c723ed58e1bbc9dd1457
normeow <rgdar@yandex.ru>
2016-07-11 17:13:30 +0300

    add imgs add login and splash screen

---
+++ app/src/main/java/californiacybertales/labraintory/SplashScreen.java
@@ -0,0 +1,32 @@
+package californiacybertales.labraintory;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreen extends AppCompatActivity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+
+}
