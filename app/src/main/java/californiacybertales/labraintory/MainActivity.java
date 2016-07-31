526448b80bf9ec94e346de2148d94a3fd48c90cd
TezRomacH <tez.romach@gmail.com>
2016-07-31 21:05:36 +0300

    navigation drawer unlocked

--- app/src/main/java/californiacybertales/labraintory/MainActivity.java
+++ app/src/main/java/californiacybertales/labraintory/MainActivity.java
@@ -44,6 +44,9 @@ public class MainActivity extends AppCompatActivity
         });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer != null) {
+            drawer.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        }
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
